efe41e4746f507f87371c3e20d1265820d979014
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-8.local>
2018-10-31 17:03:56 +0900

    almost completed

--- face_mandara_v9_7_ubuntu.py
+++ face_mandara_v9_7_ubuntu.py
@@ -376,7 +376,7 @@ if __name__ == '__main__':
                                 draw = ImageDraw.Draw(frame)
                                 radius = similar_windows[i][j].get_radius()
                                 font = ImageFont.truetype("arial.ttf", int(radius*(1/3)))
-                                d = round(distance[i][j], 5)
+                                d = round(distance[j], 5)
                                 print("d:{}".format(d))
                                 draw.text((int(x-radius*1.5+1), y+radius+1), str((1-d)*100)+"%", font=font, fill=(0,0,0,128))
                                 draw.text((int(x-radius*1.5+1), y+radius-1), str((1-d)*100)+"%", font=font, fill=(0,0,0,128))
